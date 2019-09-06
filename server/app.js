df735d0f9403866e3cdcc4191afe316e98d03eb9
Miroslav Oleinik <ollmirik@gmail.com>
2019-09-06 13:53:05 +0300

    Add files via upload

--- server/app.js
+++ server/app.js
@@ -1,12 +1,18 @@
 let express = require('express');
-const enableWs = require('express-ws')
 let app = express();
-enableWs(app);
+const enableWs = require('express-ws')(app);
 const port = 3000;
 
-app.ws('/api', (ws, req) => {
-  ws.on('message', msg => {
-    const initCurrency = {
+app.get('/api', function(req, res) {
+  res.send('Work!');
+});
+
+app.ws('/api', (webs, req) => {
+  webs.on('open', () => {
+    console.log('Opened');
+  });
+  webs.on('message', (msg) => {
+    let initCurrency = {
       base: 0,
       exchangeRates: {
         dollarCourse: Math.round((Math.random() * (3 - 2) + 2) * 100) / 100,
@@ -14,14 +20,8 @@ app.ws('/api', (ws, req) => {
         poundCourse: Math.round((Math.random() * (3 - 2) + 2) * 100) / 100,
       }
     }
-    switch (msg) {
-    case 'UPDATE':
-        ws.send(JSON.stringify(initCurrency));
-      break;
-    default:
-        ws.send(JSON.stringify(initCurrency));
-      break;
-    }
+    webs.send('Sending...');
+    webs.send(JSON.stringify(initCurrency));
   });
 });
 
