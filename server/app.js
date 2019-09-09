47035ea6a9ef0b5558324979f547f538d58307b1
Miroslav Oleinik <ollmirik@gmail.com>
2019-09-09 10:30:55 +0300

    Add files via upload

--- server/app.js
+++ server/app.js
@@ -1,25 +1,53 @@
-let express = require('express');
-let app = express();
+const express = require('express');
+const events = require('events');
+const app = express();
 const enableWs = require('express-ws')(app);
 const port = 3000;
+
+const event = new events.EventEmitter();
+
+let initCurrency = {
+  base: 0,
+  exchangeRates: {
+    dollarCourse: Math.round((Math.random() * (3 - 2) + 2) * 100) / 100,
+    euroCourse: Math.round((Math.random() * (3 - 2) + 2) * 100) / 100,
+    poundCourse: Math.round((Math.random() * (3 - 2) + 2) * 100) / 100,
+  },
+  type: 'baseCourse',
+}
 
 app.get('/api', function(req, res) {
   res.send('Work!');
 });
 
-app.ws('/api', (webs, req) => {
-  webs.on('open', () => {
-    console.log('Opened');
-  });
-  webs.on('message', (msg) => {
-    let initCurrency = {
-      base: 0,
-      exchangeRates: {
+event.on('generateCourses', function (data) {
+  setInterval(() => {
+    const randomCurrency = [
+      {
+        type: 'dollarCourse',
         dollarCourse: Math.round((Math.random() * (3 - 2) + 2) * 100) / 100,
+      },
+      {
+        type: 'euroCourse',
         euroCourse: Math.round((Math.random() * (3 - 2) + 2) * 100) / 100,
+      },
+      {
+        type: 'poundCourse',
         poundCourse: Math.round((Math.random() * (3 - 2) + 2) * 100) / 100,
-      }
-    }
+      },
+    ];
+    data.send(JSON.stringify(randomCurrency[parseInt(Math.random() * (3 - 0) + 0)]));
+  }, 10000);
+});
+
+enableWs.getWss().on('connection', (webs) => {
+  webs.send('Sending...');
+  webs.send(JSON.stringify(initCurrency));
+  event.emit('generateCourses', webs);
+});
+
+app.ws('/api', (webs, req) => {
+  webs.on('message', (msg) => {
     webs.send('Sending...');
     webs.send(JSON.stringify(initCurrency));
   });
